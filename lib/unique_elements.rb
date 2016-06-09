8845e58024f232da745c707c88775c4aa8f8c783
Rachel Lyons <rlyons21@hotmail.com>
2016-06-09 01:15:22 +0000

    submitting work for assignment

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,13 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+#  Count how many times each element appears in the array, if an element appears
+#   more than once, delete that element and any elements that are equal to it.
+    
+  arr.each do |i|
+	if arr.count(i) > 1
+		arr.delete(i)
+	end
+  end
+  return arr
 end
